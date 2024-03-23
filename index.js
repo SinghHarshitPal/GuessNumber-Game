49e6bc0a0d2a3290b7c83539f64eb560f47fc0ab
SinghHarshitPal <harshitpal713@gmail.com>
2024-03-23 11:54:30 +0530

    qq

--- index.js
+++ index.js
@@ -38,6 +38,6 @@ document.querySelector(".again").addEventListener("click", function () {
   document.querySelector(".number").textContent = "?";
   document.querySelector(".guess").value = "";
   document.querySelector("body").style.backgroundColor = "#222";
-  document.querySelector(".number").style.width = "15 rem";
+  document.querySelector(".number").style.width = "15rem";
   document.querySelector(".score").textContent = score;
 });
